78c3dd92dc7416bc91e2b57f9d8495c6dab6ec64
cbh-mavim <cbh.mavim@gmail.com>
2026-01-09 15:55:05 +0530

    Added a better todo

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,12 +1,27 @@
-user_prompt = "Enter a todo: "
-todos_list = []
+todos = []
 
 while True:
-    todo = input(user_prompt)
-    print(todo.capitalize())
-    if todo == "Exit":
-        break
-    todos_list.append(todo)
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.strip()
 
-print(todos_list)
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for index,items in enumerate(todos):
+                print(f"{index+1}.{items}")
+        case 'edit':
+            number = int(input("Enter the number of todo you wanna change: "))
+            todos[number-1] = input("Enter the new todo: ")
+        case 'complete':
+            number = int(input("Enter the todo number to mark the todo complete: "))
+            todos.pop(number-1)
+            for index,items in enumerate(todos):
+                print(f"{index}.{items}")
+        case 'exit':
+            break
+        case whatever:
+            print("You have entered the wrong choice!")
 
+print("Bye!!")
